32696394a2294a5bc901231c5886a422bd64687a
bxadam <bx.adam03@gmail.com>
2024-08-08 18:04:00 -0400

    (review) remove hardcoded values and clean up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -50,7 +50,7 @@ function App() {
       <ModalWithForm
         buttonText="Add garment"
         title="New garment"
-        activeModal={activeModal}
+        isOpen={activeModal === "add-garment"}
         onClose={closeActiveModal}
       >
         <label htmlFor="name" className="modal__label">
@@ -74,15 +74,30 @@ function App() {
         <fieldset className="modal__radio-buttons">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label_type_radio">
-            <input type="radio" className="modal__radio-input" id="hot" />
+            <input
+              type="radio"
+              name="weather"
+              className="modal__radio-input"
+              id="hot"
+            />
             Hot
           </label>
           <label htmlFor="warm" className="modal__label_type_radio">
-            <input type="radio" className="modal__radio-input" id="warm" />
+            <input
+              type="radio"
+              name="weather"
+              className="modal__radio-input"
+              id="warm"
+            />
             Warm
           </label>
           <label htmlFor="cold" className="modal__label_type_radio">
-            <input type="radio" className="modal__radio-input" id="cold" />
+            <input
+              type="radio"
+              name="weather"
+              className="modal__radio-input"
+              id="cold"
+            />
             Cold
           </label>
         </fieldset>
