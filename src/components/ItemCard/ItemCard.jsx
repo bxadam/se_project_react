1a6722c12600ec55b11135626ec03a31ce8224fd
bxadam <bx.adam03@gmail.com>
2024-08-07 23:00:18 -0400

    (feat) add modal with open and close function

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,10 +2,10 @@ import "./ItemCard.css";
 
 function ItemCard({ item }) {
   return (
-    <div className="item__container">
-      <h2 className="item__name">{item.name}</h2>
-      <img className="item__image" src={item.link} alt={item.name} />
-    </div>
+    <li className="card">
+      <h2 className="card__title">{item.name}</h2>
+      <img className="card__image" src={item.link} alt={item.name} />
+    </li>
   );
 }
 
