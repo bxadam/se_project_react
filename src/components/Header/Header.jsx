122b69afe98a6b6ee1e3aaa781619f5e7fbee5ea
bxadam <bx.adam03@gmail.com>
2024-08-16 18:30:39 -0400

    (feat) add toggle button for temperature unit selection

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -13,6 +14,9 @@ function Header({ handleAddClick, weatherData }) {
       <p className="header__date-location">
         {currentDate}, {weatherData.city}
       </p>
+      <div className="header__toggle">
+        <ToggleSwitch />
+      </div>
       <button
         onClick={handleAddClick}
         type="button"
@@ -20,6 +24,7 @@ function Header({ handleAddClick, weatherData }) {
       >
         + Add Clothes
       </button>
+
       <div className="header__user-container">
         <p className="header__username">Terrance Tegegne</p>
         <img src={avatar} alt="Terrance Tegegne" className="header__avatar" />
---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import "./ToggleSwitch.css";
+
+export default function ToggleSwitch() {
+  const [currentTempUnit, setCurrentTempUnit] = useState("F");
+
+  function handleChange() {
+    if (currentTempUnit === "C") setCurrentTempUnit("F");
+    if (currentTempUnit === "F") setCurrentTempUnit("C");
+    console.log(currentTempUnit);
+  }
+
+  return (
+    <label className="switch__container">
+      <input
+        name="switch"
+        type="checkbox"
+        className="switch__btn"
+        onChange={handleChange}
+      />
+      <span
+        className={
+          currentTempUnit === "F"
+            ? "switch__slider-F switch__slider"
+            : "switch__slider-C switch__slider"
+        }
+      />
+      <p
+        className={`switch__temp-F ${
+          currentTempUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          currentTempUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+}
